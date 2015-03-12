f1b6e2cb734923d5f95c163887874c529bb8d2e0
Hanok <hanok_k92@yahoo.co.in>
2015-03-12 07:34:45 +0530

    few changes but not final

--- proposed.py
+++ proposed.py
@@ -6,7 +6,7 @@ import ExtendedEuclidean
 import hashlib
 import time
 import timeit
-from time import gmtime, strftime
+from time import gmtime,strftime
 
 __p__=10193
 __q__=10181
@@ -15,33 +15,39 @@ totient=(__p__-1)*(__q__-1)
 e=PrimeDivisor.Prime(totient-1)
 d=ExtendedEuclidean.ExtendedEuclidean(e,totient)
 
-Message="123456789"
+Message="123456"
 
 def signature(Message):
     global __n__,d
     Message=int(Message)
-    u=random.randint(1,__n__-1)
-    x=random.randint(1,__n__-1)
+    u=77831200
+    x=69183289
     y=FastExponentiation.SquareAndMultiply(2,x,__n__)
-    #print y
+    print y
     s=FastExponentiation.SquareAndMultiply(y,d,__n__)
-    digest=(Message*FastExponentiation.SquareAndMultiply(2,u,__n__))%__n__
+    print FastExponentiation.SquareAndMultiply(s,e,__n__)
+    digest=((Message)*FastExponentiation.SquareAndMultiply(2,u,__n__))%__n__
+    print digest
     r1=(s+digest)%__n__
+    print (r1-s)%__n__
     TSS(s,r1,u,x,y)
 
 def finalsignature(s,r,r1,r2,t,u,x,y):
     global __n__
-    invTemp=FastExponentiation.SquareAndMultiply(x,__n__-2,__n__)
+    invTemp=ExtendedEuclidean.ExtendedEuclidean(x,__n__)
+    print invTemp
+    print "r1="+str(r1)+"r2="+str(r2)
     l=(invTemp*(r2-u+r1))%__n__
+    print "l="+str(l)
     verification(s,y,l,r,t)
 
-
 def TSS(s,r1,u,x,y):
-    t=987654321
-    k=random.randint(1,__n__-1)
+    t=98765
+    k=51887467
     r2=(k-r1-t)%__n__
     temp=FastExponentiation.SquareAndMultiply(2,k,__n__)
-    r=((r1-s)*(FastExponentiation.SquareAndMultiply(temp,__n__-2,__n__)))%__n__
+    r=((r1-s)*ExtendedEuclidean.ExtendedEuclidean(temp,__n__))%__n__
+    print "r="+str(r)
     finalsignature(s,r,r1,r2,t,u,x,y)
 
 def verification(s,y,l,r,t):
